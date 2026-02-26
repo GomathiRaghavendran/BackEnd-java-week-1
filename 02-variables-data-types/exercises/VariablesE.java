eaff47645bd477d3431893d80b8e3d608fd6ccc7
eesha <Gomathi.Balasubramanianmvs@gmail.com>
2026-02-26 10:27:25 +0100

    Variable exercise is done

--- 02-variables-data-types/exercises/VariablesE.java
+++ 02-variables-data-types/exercises/VariablesE.java
@@ -29,21 +29,43 @@ public class VariablesE
         System.out.println("Double: " + myDecimal);
         System.out.println("Boolean: " + myBoolean);
 
+
         // -------------------- Student Exercises --------------------
         // 1. Create a char variable called myLetter and assign it any letter
+        char myLetter='A';
+        System.out.println("char: " + myLetter);
 
         // 2. Create a float variable called myFloat and assign it any decimal
+        float myFloat=99.35f;
+        System.out.println("float: "+myFloat);
 
         // 3. Create a long variable called myBigNumber and assign it a large number
+        long myBigNumber=1000000000l;
+        System.out.println("long: "+myBigNumber);
 
         // 4. Create a byte variable called mySmallNumber and assign it a small number
+        byte mySmallNumber=127;
+        System.out.println("byte: "+mySmallNumber);
 
         // 5. Create a short variable called myShortNumber and assign it a number
+        short myShortNumber=32767;
+        System.out.println("short: "+myShortNumber);
 
         // 6. Change the value of myNumber and print the new value
+        int myNumber1=25;
+        System.out.println("int: "+myNumber);
 
         // 7. Create two int variables, add them, and print the result
 
+        int a=50; int b=50;
+        int sum=a+b;
+        System.out.println("sum of a & b: "+ sum);
+
+
         // 8. Create a String variable for your name and print a greeting message
+
+        String myName1="Gomathi";
+        String greeting1="Have a Fantastic day!";
+        System.out.println("Concatenating the String: "+(myName1 +","+ greeting1));
     }
 }
